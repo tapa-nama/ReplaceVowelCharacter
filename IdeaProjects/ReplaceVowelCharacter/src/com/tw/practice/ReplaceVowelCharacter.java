0ca0a3b04a4d69b0844cd70c2538663c556fb91e
tapa-nama <zhoutiian@gmail.com>
2018-01-18 21:57:42 +0800

    version 0.0

---
+++ IdeaProjects/ReplaceVowelCharacter/src/com/tw/practice/ReplaceVowelCharacter.java
@@ -0,0 +1,4 @@
+package com.tw.practice;
+
+public class ReplaceVowelCharacter {
+}
---
+++ IdeaProjects/ReplaceVowelCharacter/test/com/tw/practice/ReplaceVowelCharacterTest.java
@@ -0,0 +1,7 @@
+package com.tw.practice;
+
+import static org.junit.Assert.*;
+
+public class ReplaceVowelCharacterTest {
+
+}
