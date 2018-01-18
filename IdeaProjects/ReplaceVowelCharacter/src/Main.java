6cd77679e3654a247ffa1c2c98cb4793213e1203
tapa-nama <zhoutiian@gmail.com>
2018-01-18 23:58:48 +0800

    Add main() method.

--- IdeaProjects/ReplaceVowelCharacter/src/Main.java
+++ IdeaProjects/ReplaceVowelCharacter/src/Main.java
@@ -1,2 +1,14 @@
+import com.tw.practice.ReplaceVowelCharacter;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+
+        ReplaceVowelCharacter replaceVowelCharacter = new ReplaceVowelCharacter();
+        String result = replaceVowelCharacter.replace(str);
+        System.out.println(result);
+    }
 }
