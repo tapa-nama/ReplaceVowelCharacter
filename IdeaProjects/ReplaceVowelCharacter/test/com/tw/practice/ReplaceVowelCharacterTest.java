b4f045ef14d138215e57e1c236db28f7c264f62f
tapa-nama <zhoutiian@gmail.com>
2018-01-18 22:05:46 +0800

    The first failure Test without implementation method.

--- IdeaProjects/ReplaceVowelCharacter/test/com/tw/practice/ReplaceVowelCharacterTest.java
+++ IdeaProjects/ReplaceVowelCharacter/test/com/tw/practice/ReplaceVowelCharacterTest.java
@@ -1,7 +1,15 @@
 package com.tw.practice;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ReplaceVowelCharacterTest {
+    private ReplaceVowelCharacter replaceVC = new ReplaceVowelCharacter();
+
+    @Test
+    public void shouldReturnMommyWhenInputAVowel() {
+        assertEquals("mommy", replaceVC.replace("a"));
+    }
 
 }
