6581ec3e270c775d97bfdc7458a647e027199000
tapa-nama <zhoutiian@gmail.com>
2018-01-18 23:39:43 +0800

    Finished three Test cases and the implementation.

--- IdeaProjects/ReplaceVowelCharacter/src/com/tw/practice/ReplaceVowelCharacter.java
+++ IdeaProjects/ReplaceVowelCharacter/src/com/tw/practice/ReplaceVowelCharacter.java
@@ -2,12 +2,32 @@ package com.tw.practice;
 
 public class ReplaceVowelCharacter {
     private String vowels = "aeiouAEIOU";
+    private final double LIMIT_PERCENT = 0.30;
 
     public String replace(String str) {
-        if (vowels.contains(str)) {
-            return "mommy";
+
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (vowels.contains("" + str.charAt(i))) {
+                count++;
+            }
         }
-        return str;
+        double percent = (double) count / str.length();
+        String result = "";
+
+        if (percent > LIMIT_PERCENT) {
+            for (int i = 0; i < str.length(); i++) {
+                if (vowels.contains("" + str.charAt(i))) {
+                    result += "mommy";
+                } else {
+                    result += str.charAt(i);
+                }
+            }
+        } else {
+            result = str;
+        }
+
+        return result;
 
     }
 }
--- IdeaProjects/ReplaceVowelCharacter/test/com/tw/practice/ReplaceVowelCharacterTest.java
+++ IdeaProjects/ReplaceVowelCharacter/test/com/tw/practice/ReplaceVowelCharacterTest.java
@@ -10,11 +10,21 @@ public class ReplaceVowelCharacterTest {
     @Test
     public void shouldReturnMommyWhenInputAVowel() {
         assertEquals("mommy", replaceVC.replace("a"));
+        assertEquals("mommy", replaceVC.replace("e"));
+        assertEquals("mommy", replaceVC.replace("i"));
+        assertEquals("mommy", replaceVC.replace("o"));
+        assertEquals("mommy", replaceVC.replace("u"));
     }
 
     @Test
     public void shouldReturnItselfWhenInputAConsonant() {
         assertEquals("b",replaceVC.replace("b"));
+    }
+
+    @Test
+    public void shouldReplaceAllVowelsWhenVowelsMoreThan30Percent() {
+        assertEquals("Shmommy",replaceVC.replace("She"));
+        assertEquals("python",replaceVC.replace("python"));
     }
 
 }
